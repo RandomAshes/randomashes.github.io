ae55842265c5c610ebb00c294ea21feca0ba3143
Ashley Clifton <randomashes@gmail.com>
2024-01-23 23:30:11 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+	output: 'export',
+	basePath: process.env.NODE_ENV === 'production' ? '/se-portfolio' : undefined,
+	experimental: {
+		appDir: true,
+	},
+	images: {
+		unoptimized: true,
+	},
+	reactStrictMode: true,
 };
 
 module.exports = nextConfig;
