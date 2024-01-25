7cb5534b4f51da04cdee5992fc3d9f381bcb5016
Ashley Clifton <randomashes@gmail.com>
2024-01-24 19:16:01 -0500

    Update home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
+import Image from 'next/image';
+
+import styles from './page.module.scss';
+
 function HomePage() {
 	return (
 		<div>
-			<h1>Home</h1>
+			<Image
+				src="/images/pexels-rizky-subagja-9053882_medium.jpg"
+				sizes="(min-width: 768px) 100vw, 50vh"
+				fill
+				alt="Picture of the author"
+				className={styles.backgroundImage}
+			/>
+			<div className={styles.content}>
+				<h1>Ashley Clifton</h1>
+				<p>Software Engineer</p>
+			</div>
 		</div>
 	);
 }
