aa97afdc16dfe885bafa70100afbc70049ceb9f9
Ashley Clifton <randomashes@gmail.com>
2024-01-24 21:38:50 -0500

    Ensure coming soon page is mobile friendly

--- src/app/_components/coming-soon/coming-soon.tsx
+++ src/app/_components/coming-soon/coming-soon.tsx
@@ -3,7 +3,7 @@ import styles from './coming-soon.module.scss';
 function ComingSoon() {
 	return (
 		<div className={styles.comingSoon}>
-			<h1>Coming Soon</h1>
+			<h1 className={styles.title}>Coming Soon</h1>
 			<p>We are working hard to bring you something awesome. Stay tuned!</p>
 		</div>
 	);
